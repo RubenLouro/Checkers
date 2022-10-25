3b67b717a766feadceee44a3586ed3ada47b5b89
ruben <rubenlouro300@hotmail.com>
2022-10-25 23:02:45 +0100

    .

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -1,17 +1,45 @@
-const val BOARD_DIM:Int = 5
+const val BOARD_DIM:Int = 8
+const val TOTAL_PIECES = 24
+
+private fun initialList():List<Move> {
+    val blackPositions = Square.values.filter { it.black && it.row.number in (BOARD_DIM - 2..BOARD_DIM) }
+    val whitePositions = Square.values.filter { it.black && it.row.number in (BOARD_DIM - BOARD_DIM..BOARD_DIM - 5) }
+    val total = blackPositions + whitePositions
+    val list = List(TOTAL_PIECES){ move->
+                if (move < TOTAL_PIECES /2) Move(Player.Black, total[move]) else Move(Player.White, total[move])
+        }
+    return list
+    }
 
 
-sealed class Board(val moves : List<Move> = emptyList()) {
+
+sealed class Board(
+    val moves : List<Move> = emptyList()
+) {
 
     companion object{
+        fun deserialize(input: String): Board {
+            val lines = input.split("\n")
+            val kind = lines[0]
+            val moves = lines.drop(1).map { Move.deserialize(it) }
+            return when (kind) {
+                BoardRun::class.simpleName -> BoardRun(moves, moves.last().player)
+                BoardDraw::class.simpleName -> BoardDraw(moves)
+                BoardWinner::class.simpleName -> BoardWinner(moves, moves.last().player)
+                else -> { throw IllegalArgumentException("There is no board type for input $kind")}
+            }
+        }
 }
     abstract fun play(player: Player, square: Square):Board
+    fun get(square: Square): Move? {
+        return moves.find { it.square == square }
+    }
 }
 
 
 class BoardDraw(moves: List<Move>) : Board(moves) {
     override fun play(player: Player, square: Square): Board {
-        throw IllegalStateException("This game has already ended on a draw")
+        throw IllegalStateException("This game has already ended on a draw!")
     }
 }
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,12 @@
-fun main(){
-    val list = listOf(1, 2, 3)
-    val list2 = list + 4
-    print(list2)
+
+
+fun printBoardCheckers(board: Board){
+
 }
+
+fun main(){
+
+printBoardCheckers(BoardRun(emptyList(),Player.Black))
+}
+
+
